50274c2f5157d25381b370ee0773ef01e9b6bbc3
MaksimOzery <boris-gudkov0@rambler.ru>
2021-07-02 20:32:53 +0300

    SmartSoft2

--- www/web-proxy-sso/src/opnsense/mvc/app/controllers/OPNsense/ProxySSO/Api/ServiceController.php
+++ www/web-proxy-sso/src/opnsense/mvc/app/controllers/OPNsense/ProxySSO/Api/ServiceController.php
@@ -1,4 +1,31 @@
 <?php
+/*
+ * Copyright (C) 2017 Smart-Soft
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 
 namespace OPNsense\ProxySSO\Api;
 
@@ -21,7 +48,7 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController
         $backend = new Backend();
 
         $response = $backend->configdRun("proxysso showkeytab");
-        return ["response" => $response, "status" => "ok"];
+        return array("response" => $response, "status" => "ok");
     }
 
     /**
@@ -33,7 +60,7 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController
         $backend = new Backend();
 
         $response = $backend->configdRun("proxysso deletekeytab");
-        return ["response" => $response, "status" => "ok"];
+        return array("response" => $response, "status" => "ok");
     }
 
     /**
@@ -60,10 +87,10 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController
 
             $response = $backend->configdRun("proxysso createkeytab {$hostname} {$domain} {$kerbname} {$winver} {$username} {$pass} {$realm}");
             parent::reconfigureAction();
-            return ["response" => $response, "status" => "ok"];
+            return array("response" => $response, "status" => "ok");
         }
 
-        return ["response" => []];
+        return array("response" => []);
     }
 
     /**
@@ -85,10 +112,10 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController
             }
 
             $response = $backend->configdRun("proxysso testkerblogin {$username} {$pass} {$fqdn}");
-            return ["response" => $response, "status" => "ok"];
+            return array("response" => $response, "status" => "ok");
         }
 
-        return ["response" => []];
+        return array("response" => []);
     }
 
     /**
@@ -307,7 +334,7 @@ class ServiceController extends \OPNsense\Proxy\Api\ServiceController
                         }
                         $domain_arr[] = explode("=", $element)[1];
                     }
-                    return [$method, implode(".", $domain_arr)];
+                    return array($method, implode(".", $domain_arr));
                 }
             }
         }
