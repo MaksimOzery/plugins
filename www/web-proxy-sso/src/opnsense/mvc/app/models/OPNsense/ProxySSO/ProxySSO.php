27a5f15ae56553f8bc2efe0cc89ea59666c2bb8f
MaksimOzery <boris-gudkov0@rambler.ru>
2021-07-07 16:11:59 +0000

    SmartSoft_13

--- www/web-proxy-sso/src/opnsense/mvc/app/models/OPNsense/ProxySSO/ProxySSO.php
+++ www/web-proxy-sso/src/opnsense/mvc/app/models/OPNsense/ProxySSO/ProxySSO.php
@@ -33,11 +33,10 @@ use OPNsense\Core\Config;
 class ProxySSO extends BaseModel
 {
     protected function init()
-	{
-	    if($this->KerberosHostName == "") {
-		    $hostname = (string)Config::getInstance()->object()->system->hostname;
-		    $this->KerberosHostName = substr(strtoupper($hostname), 0, 13) . '-K';
-	    }
+    {
+        if ($this->KerberosHostName == "") {
+            $hostname = (string)Config::getInstance()->object()->system->hostname;
+            $this->KerberosHostName = substr(strtoupper($hostname), 0, 13) . '-K';
+        }
     }
 }
-
